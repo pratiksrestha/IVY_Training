57deefae79a4ba1ecd070ee821917ccca8b49e62
pratiksrestha <ps2020.official@gmail.com>
2022-09-16 11:11:51 +0530

    excluding custom wrapper and custom exception class

--- IVY_Training/src/Leap_Year.java
+++ IVY_Training/src/Leap_Year.java
@@ -1,6 +1,30 @@
-//import java.util.*;
-//
-//public class Leap_Year {
-//    public static void main(String[] args){
-//
-//}
+import java.util.*;
+
+public class Leap_Year {
+    public static void main(String[] args) {
+
+        int year;
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter the year you want to check whether it is a leap year or not::");
+        year = s.nextInt();
+        boolean leap = false;
+
+        if (year % 4 == 0) {
+            if (year % 100 == 0) {
+                if (year % 400 == 0)
+                    leap = true;
+                else
+                    leap = false;
+            }
+            else
+                leap = true;
+        }
+        else
+            leap = false;
+
+        if (leap)
+            System.out.println(year + " is a leap year.");
+        else
+            System.out.println(year + " is not a leap year.");
+    }
+}
--- IVY_Training/src/Switch_Case.java
+++ IVY_Training/src/Switch_Case.java
@@ -1,2 +1,20 @@
+import java.util.*;
 public class Switch_Case {
+    public static void main(String args[]){
+        int num;
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter the case number:");
+        num = s.nextInt();
+        switch(num)
+        {
+            case '1':
+                System.out.println("Case1: Value is: "+num);
+            case '2':
+                System.out.println("Case2: Value is: "+num);
+            case '3':
+                System.out.println("Case3: Value is: "+num);
+            default:
+                System.out.println("Default: Value is: "+num);
+        }
+    }
 }
